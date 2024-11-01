d529f3ccd9cd992c4360552db143391a7f23a03e
TaynaCandidaSilva~ <tayna2507@gmail.com>
2024-11-01 19:47:57 -0300

    Finalizando projeto da primeira calculadora

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,13 +1,12 @@
 from flask import Blueprint, jsonify, request
 from src.calculators.calculator_1 import Calculator1
 
-
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
-def calculator_1():
+def calculator1():
     calc = Calculator1()
-    calc.calculate(request)
+    response = calc.calculate(request)
 
-    return jsonify({"sucess": True}), 200
+    return jsonify(response), 200
